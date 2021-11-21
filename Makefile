1c878db035ea259e1def4f3f545277f9f68cc0b5
Moritz Wilksch <moritzwilksch@gmail.com>
2021-11-21 20:37:42 +0000

    basics

---
+++ Makefile
@@ -0,0 +1,2 @@
+get-data:
+	wget https://files.pushshift.io/reddit/69M_reddit_accounts.csv.gz -O data/reddit_accounts.csv.gz
---
+++ basics.py
@@ -0,0 +1,56 @@
+#%%
+import polars as pl
+import re
+
+re_cashtags = re.compile(r"\$[A-Z]+")
+
+df: pl.DataFrame = pl.scan_csv("data/TSLA_tweets.csv")
+# df: pl.DataFrame = pl.read_csv("data/TSLA_tweets.csv")
+
+#%%
+df.head(1)
+
+#%%
+def fix_datetime(dataf: pl.DataFrame) -> pl.DataFrame:
+    return dataf.with_column(
+        pl.col("created_at").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S %Z")
+    ).drop(["date", "time", "timezone"])
+
+
+def clean_tweet(dataf: pl.DataFrame) -> pl.DataFrame:
+    return dataf.with_column(pl.col("tweet").str.replace("@[\w_\d]+", "@user"))
+
+
+def clean_vector(dataf: pl.DataFrame) -> pl.DataFrame:
+    url_regex = r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)"
+    return dataf.with_column(pl.col("tweet").str.replace(url_regex, ""))
+
+
+def get_cashtags(s: str) -> list:
+    return re_cashtags.findall(s)
+
+
+def extract_cashtags(dataf: pl.DataFrame) -> pl.DataFrame:
+    return dataf.with_column(pl.col("tweet").apply(get_cashtags))
+
+
+#%%
+clean: pl.DataFrame = df.pipe(fix_datetime).pipe(clean_tweet).pipe(clean_vector)
+# clean = clean.shrink_to_fit()
+
+
+#%%
+def list_from_strlist(s: str) -> int:
+    return len(s.strip("[]").replace("'", "").split(","))
+
+
+# count n_cashtags
+clean = clean.with_column(pl.col("cashtags").apply(list_from_strlist).alias("n_cashtags")).collect()
+
+#%%
+clean.select(("cashtags", "n_cashtags"))
+
+#%%
+clean.with_column(pl.col("username").cast(pl.Categorical).alias("username_cat")).groupby(
+    "username_cat"
+).agg(pl.col("id").n_unique()).sort("id_n_unique")
