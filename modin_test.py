a078cee2b58fc83f202f9889d5c2ce69d3e3caac
Moritz Wilksch <moritzwilksch@gmail.com>
2021-11-23 07:34:04 -0800

    modin test

---
+++ modin_test.py
@@ -0,0 +1,84 @@
+#%%
+import ray
+ray.init()
+import modin.pandas as pd
+
+
+#%%
+
+keep_cols = [
+    "id",
+    "created_at",
+    "date",
+    "time",
+    "user_id",
+    "username",
+    "name",
+    "tweet",
+    "language",
+    "replies_count",
+    "retweets_count",
+    "likes_count",
+    "hashtags",
+    "cashtags",
+    "link",
+]
+
+
+def fix_dtypes(data: pd.DataFrame) -> pd.DataFrame:
+    print(f"[PROCESSING] Fixing dtypes...")
+    data = data.assign(created_at=pd.to_datetime(data.date + " " + data.time))
+    data = data.drop(["date", "time"], axis=1)
+
+    string_cols = ["username", "name", "tweet", "link"]
+    data[string_cols] = data[string_cols].astype("string")
+
+    data["language"] = data["language"].astype("category")
+    return data
+
+
+def clean_object_cols(data: pd.DataFrame) -> pd.DataFrame:
+    print(f"[PROCESSING] Cleaning obj-cols...")
+    data["hashtags"] = data.hashtags.apply(
+        lambda x: ", ".join(x.split("', '")).strip("[']")
+    ).astype("string")
+    data.loc[data.hashtags.str.len() == 0, "hashtags"] = pd.NA
+
+    data["cashtags"] = data.cashtags.apply(
+        lambda x: ", ".join(x.split("', '")).strip("[']")
+    ).astype("string")
+    data.loc[data.cashtags.str.len() == 0, "cashtags"] = pd.NA
+
+    return data
+
+
+def drop_dupes(data: pd.DataFrame) -> pd.DataFrame:
+    """Drop dupes."""
+    len_before = len(data)
+    data = data.drop_duplicates(subset="id")
+    len_after = len(data)
+
+    print(f"[PROCESSING] Dropped {len_before - len_after} duplicates...")
+
+    return data
+
+
+
+
+
+#%%
+%%time
+df = pd.read_csv("TSLA_tweets.csv")
+
+
+
+
+
+#%%
+%%time
+clean: pd.DataFrame = (
+    df.pipe(fix_dtypes)
+    .pipe(drop_dupes)
+    .pipe(clean_object_cols)
+    # .query("language == 'en'") # filtered later
+)
--- na_table.py
+++ na_table.py
@@ -10,6 +10,4 @@ df = pl.read_parquet(
 maskdf = df.with_columns(
     [pl.col(c).is_null().cast(pl.UInt16).alias(f"{c}_null") for c in df.columns]
 ).select("^.*_null$")
-maskdf.melt(maskdf.columns, maskdf.columns).groupby("variable").pivot(
-    "variable", "value"
-).sum()
+maskdf.melt(maskdf.columns, maskdf.columns).groupby("variable").pivot("variable", "value").sum()
