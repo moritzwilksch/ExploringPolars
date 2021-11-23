645d0fb9e1fb9350b88a7e40b2f675b230bed309
Moritz Wilksch <moritzwilksch@gmail.com>
2021-11-22 21:02:27 -0800

    na table example

---
+++ na_table.py
@@ -0,0 +1,15 @@
+#%%
+import polars as pl
+
+#%%
+df = pl.read_parquet(
+    "https://github.com/moritzwilksch/BerlinRealEstatePrices/raw/main/data/berlin_clean.parquet"
+)
+
+#%%
+maskdf = df.with_columns(
+    [pl.col(c).is_null().cast(pl.UInt16).alias(f"{c}_null") for c in df.columns]
+).select("^.*_null$")
+maskdf.melt(maskdf.columns, maskdf.columns).groupby("variable").pivot(
+    "variable", "value"
+).sum()
