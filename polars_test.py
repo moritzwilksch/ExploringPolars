af6a8e331583b15b336af077e92dfc8c4de163f4
Moritz Wilksch <moritzwilksch@gmail.com>
2021-11-28 14:12:58 -0800

    minor fixes but WIP

--- polars_test.py
+++ polars_test.py
@@ -24,7 +24,6 @@ keep_cols = [
 ]
 
 #%%
-%%time
 df = pl.scan_csv(root_path + f"{ticker}_tweets.csv").select(pl.col(keep_cols))
 
 
@@ -32,32 +31,27 @@ df = pl.scan_csv(root_path + f"{ticker}_tweets.csv").select(pl.col(keep_cols))
 def fix_dtypes(data):
     print(f"[PROCESSING] Fixing dtypes...")
     # data = data.assign(created_at=pl.to_datetime(data.date + " " + data.time))
-    data = data.with_column(pl.col("created_at").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"))
-    data = data.drop(["date", "time"])
-
     string_cols = ["username", "name", "tweet", "link"]
-    data = data.with_column(pl.col(string_cols).cast(pl.Utf8))
+    data = (
+        data.with_column(pl.col("created_at").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"))
+        .drop(["date", "time"])
+        .with_column(pl.col(string_cols).cast(pl.Utf8))
+        .with_column(pl.col("language").cast(pl.Categorical))
+    )
 
-    data = data.with_column(pl.col("language").cast(pl.Categorical))
     return data
 
 
 def clean_object_cols(data):
     print(f"[PROCESSING] Cleaning obj-cols...")
-    # data["hashtags"] = data.hashtags.apply(
-    #     lambda x: ", ".join(x.split("', '")).strip("[']")
-    # ).astype("string")
 
-    data = data.with_column(pl.col("hashtags").apply(lambda x: ", ".join(x.split("', '")).strip("[']")).cast(pl.Utf8))
-    # data = data.filter(pl.col("hashtags").apply(lambda x: len(x)) == 0).select("hashtags").set(np.nan)
-    data.with_column(pl.when(pl.col("hashtags").eq(0), np.nan).otherwise(pl.col("hashtags")))
-
-    # data.loc[data.hashtags.str.len() == 0, "hashtags"] = pd.NA
-
-    # data["cashtags"] = data.cashtags.apply(
-    #     lambda x: ", ".join(x.split("', '")).strip("[']")
-    # ).astype("string")
-    # data.loc[data.cashtags.str.len() == 0, "cashtags"] = pd.NA
+    for objcol in ["hashtags", "cashtags"]:
+        data = data.with_column(
+            pl.col(objcol).apply(lambda x: x.strip("[']").split("', '"))
+        )
+        data.with_column(
+            pl.when(pl.col(objcol).eq(pl.lit(0))).then(np.nan).otherwise(pl.col(objcol))
+        )
 
     return data
 
@@ -73,11 +67,17 @@ def drop_dupes(data):
 
     return data
 
+
 #%%
-%%time
 clean = (
     df.pipe(fix_dtypes)
     .pipe(drop_dupes)
     .pipe(clean_object_cols)
     # .query("language == 'en'") # filtered later
 ).collect()
+
+print("Done!")
+
+
+#%%
+clean.apply(lambda x: len(x.select("hashtags")))
